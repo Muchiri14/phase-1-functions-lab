17187c6230127bc02c5d9893b3c629d9e276117e
Daniel <muchiridaniel59@gmail.com>
2022-11-24 17:49:24 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(value) {
+    if (value == 43) {
+    return (1);
+    }
+    else {
+        return (8);
+    }
+}
+function distanceFromHqInFeet(location) {
+    if (location == 43) {
+        return (264);
+    }
+    else {
+        return (2112);
+    }
+}
+function distanceTravelledInFeet(x, y) {
+    if (y > x) {
+        const walking = (y - x) * 264;
+        return (walking);
+    }
+    else if (x > y) {
+        const walking = (x - y) * 264;
+        return (walking);
+    }
+}
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination)
+    let n;
+    if (distance <= 400) {
+        n = 0;
+    }
+    else if (distance > 400 && distance < 2000) {
+        n = 2.56;
+    }
+    else if (distance > 2000 && distance < 2500) {
+        n = 25;
+    }
+    else if (distance > 2500) {
+        n = 'cannot travel that far';
+    }
+    return n;
+}
